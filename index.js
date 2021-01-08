7f340f34f961b477787880119aafa0b6c06cb477
Tom Xu <ycxzhkx@gmail.com>
2021-01-08 16:44:24 +0800

    fix: android release

--- index.js
+++ index.js
@@ -1,244 +1,318 @@
-import {DeviceEventEmitter, NativeEventEmitter, NativeModules, Platform} from "react-native";
-const {RNAliyunOSS} = NativeModules;
+import {
+  DeviceEventEmitter,
+  NativeEventEmitter,
+  NativeModules,
+  Platform,
+} from 'react-native';
+const { RNAliyunOSS } = NativeModules;
 
 let subscription;
 
 //default configuration for OSS Client
 const conf = {
-    maxRetryCount: 3,
-    timeoutIntervalForRequest: 30,
-    timeoutIntervalForResource: 24 * 60 * 60
+  maxRetryCount: 3,
+  timeoutIntervalForRequest: 30,
+  timeoutIntervalForResource: 24 * 60 * 60,
 };
 
 const imageXOssProcess = {
-    "x-oss-process":''
-}
+  'x-oss-process': '',
+};
 
-let partSize = 128 * 1024
+let partSize = 128 * 1024;
 const mulitpartUploadConfig = {
-    "partSize":partSize
-}
+  partSize: partSize,
+};
 
 //appendObject
 const appendOptions = {
-    "appendPosition":0,
-    "contentType":'',
-    "contentMd5":'',
-    "contentEncoding":'',
-    "contentDisposition":''
-}
+  appendPosition: 0,
+  contentType: '',
+  contentMd5: '',
+  contentEncoding: '',
+  contentDisposition: '',
+};
 
 export default AliyunOSS = {
+  //Enable dev mode
+  enableDevMode() {
+    RNAliyunOSS.enableDevMode();
+  },
 
-    //Enable dev mode
-    enableDevMode() {
-        RNAliyunOSS.enableDevMode();
-    },
-    
-    /**
-     * Initialize the OSS Client
-     * Mode: PlainTextAKSK
-     */
-    initWithPlainTextAccessKey(accessKey, secretKey, endPoint, configuration = conf) {
-        RNAliyunOSS.initWithPlainTextAccessKey(accessKey, secretKey, endPoint, configuration);
-    },
+  /**
+   * Initialize the OSS Client
+   * Mode: PlainTextAKSK
+   */
+  initWithPlainTextAccessKey(
+    accessKey,
+    secretKey,
+    endPoint,
+    configuration = conf,
+  ) {
+    RNAliyunOSS.initWithPlainTextAccessKey(
+      accessKey,
+      secretKey,
+      endPoint,
+      configuration,
+    );
+  },
 
-    /**
-     * Initialize the OSS Client
-     * Mode: ImplementedSigner
-     */
-    initWithImplementedSigner(signature, accessKey, endPoint, configuration = conf) {
-        RNAliyunOSS.initWithImplementedSigner(signature, accessKey, endPoint, configuration);
-    },
+  /**
+   * Initialize the OSS Client
+   * Mode: ImplementedSigner
+   */
+  initWithImplementedSigner(
+    signature,
+    accessKey,
+    endPoint,
+    configuration = conf,
+  ) {
+    RNAliyunOSS.initWithImplementedSigner(
+      signature,
+      accessKey,
+      endPoint,
+      configuration,
+    );
+  },
 
-    /**
-     * Initialize the OSS Client
-     * Mode: SecurityToken (STS)
-     */
-    initWithSecurityToken(securityToken, accessKey, secretKey, endPoint, configuration = conf) {
-        RNAliyunOSS.initWithSecurityToken(securityToken, accessKey, secretKey, endPoint, configuration);
-    },
+  /**
+   * Initialize the OSS Client
+   * Mode: SecurityToken (STS)
+   */
+  initWithSecurityToken(
+    securityToken,
+    accessKey,
+    secretKey,
+    endPoint,
+    configuration = conf,
+  ) {
+    RNAliyunOSS.initWithSecurityToken(
+      securityToken,
+      accessKey,
+      secretKey,
+      endPoint,
+      configuration,
+    );
+  },
 
-    /**
-     * Initialize the OSS Client
-     * Server STS
-     */
-    initWithServerSTS(server, endPoint, configuration = conf) {
-        RNAliyunOSS.initWithServerSTS(server, endPoint, configuration);
-    },    
+  /**
+   * Initialize the OSS Client
+   * Server STS
+   */
+  initWithServerSTS(server, endPoint, configuration = conf) {
+    RNAliyunOSS.initWithServerSTS(server, endPoint, configuration);
+  },
 
-    /**
-     * Asynchronously uploading
-     */
-    asyncUpload(bucketName, objectKey, filepath,options) {
-        return RNAliyunOSS.asyncUpload(bucketName, objectKey, filepath,options);
-    },
+  /**
+   * Asynchronously uploading
+   */
+  asyncUpload(bucketName, objectKey, filepath, options) {
+    return RNAliyunOSS.asyncUpload(bucketName, objectKey, filepath, options);
+  },
 
-     /**
-     * Asynchronously 
-     */
-    asyncResumableUpload(bucketName, objectKey, filepath='',options={}) {
-        return RNAliyunOSS.asyncResumableUpload(bucketName, objectKey, filepath,options);
-    },
+  /**
+   * Asynchronously
+   */
+  asyncResumableUpload(bucketName, objectKey, filepath = '', options = {}) {
+    return RNAliyunOSS.asyncResumableUpload(
+      bucketName,
+      objectKey,
+      filepath,
+      options,
+    );
+  },
 
-     /**
-     * Asynchronously asyncAppendObject
-     */
-    asyncAppendObject(bucketName,objectKey,filepath,options = appendOptions) {
-        return RNAliyunOSS.asyncAppendObject(bucketName, objectKey, filepath,options);
-    },
+  /**
+   * Asynchronously asyncAppendObject
+   */
+  asyncAppendObject(bucketName, objectKey, filepath, options = appendOptions) {
+    return RNAliyunOSS.asyncAppendObject(
+      bucketName,
+      objectKey,
+      filepath,
+      options,
+    );
+  },
 
-    /**
-     * Asynchronously 
-     */
-    initMultipartUpload(bucketName,objectKey) {
-        return RNAliyunOSS.initMultipartUpload(bucketName, objectKey);
-    },
+  /**
+   * Asynchronously
+   */
+  initMultipartUpload(bucketName, objectKey) {
+    return RNAliyunOSS.initMultipartUpload(bucketName, objectKey);
+  },
 
-    /**
-     * Asynchronously multipartUpload
-     */
-    multipartUpload(bucketName,objectKey,uploadId,filepath ='',options = mulitpartUploadConfig) {
-        return RNAliyunOSS.multipartUpload(bucketName, objectKey, uploadId,filepath, options);
-    },
+  /**
+   * Asynchronously multipartUpload
+   */
+  multipartUpload(
+    bucketName,
+    objectKey,
+    uploadId,
+    filepath = '',
+    options = mulitpartUploadConfig,
+  ) {
+    return RNAliyunOSS.multipartUpload(
+      bucketName,
+      objectKey,
+      uploadId,
+      filepath,
+      options,
+    );
+  },
 
-     /**
-     * Asynchronously listParts
-     */
-    listParts (bucketName,objectKey,uploadId) {
-        return RNAliyunOSS.listParts(bucketName, objectKey, uploadId)
-    },
-     /**
-     * Asynchronously abortMultipartUpload
-     */
-    abortMultipartUpload(bucketName,objectKey,uploadId) {
-        return RNAliyunOSS.abortMultipartUpload(bucketName, objectKey, uploadId);
-    },
+  /**
+   * Asynchronously listParts
+   */
+  listParts(bucketName, objectKey, uploadId) {
+    return RNAliyunOSS.listParts(bucketName, objectKey, uploadId);
+  },
+  /**
+   * Asynchronously abortMultipartUpload
+   */
+  abortMultipartUpload(bucketName, objectKey, uploadId) {
+    return RNAliyunOSS.abortMultipartUpload(bucketName, objectKey, uploadId);
+  },
 
-    /**
-     * Asynchronously downloading
-     */
-    asyncDownload(bucketName, objectKey, filepath='',options = imageXOssProcess) {
-        return RNAliyunOSS.asyncDownload(bucketName, objectKey, filepath,options);
-    },
-    
-    /*
+  /**
+   * Asynchronously downloading
+   */
+  asyncDownload(
+    bucketName,
+    objectKey,
+    filepath = '',
+    options = imageXOssProcess,
+  ) {
+    return RNAliyunOSS.asyncDownload(bucketName, objectKey, filepath, options);
+  },
+
+  /*
     asyncListBuckets
     */
 
-    asyncListBuckets () {
-        return RNAliyunOSS.asyncListBuckets()
-    },
+  asyncListBuckets() {
+    return RNAliyunOSS.asyncListBuckets();
+  },
 
-     /**
-     * Asynchronously getHeadObject
-     */
+  /**
+   * Asynchronously getHeadObject
+   */
 
-    asyncHeadObject (bucketName, objectKey) {
-        return RNAliyunOSS.asyncHeadObject(bucketName,objectKey)
-    }, 
+  asyncHeadObject(bucketName, objectKey) {
+    return RNAliyunOSS.asyncHeadObject(bucketName, objectKey);
+  },
 
-    
-    /**
-     * Asynchronously getAsyncObjects
-     */
+  /**
+   * Asynchronously getAsyncObjects
+   */
 
-    asyncListObjects (bucketName,options) {
-        return RNAliyunOSS.asyncListObjects(bucketName,options)
-    },
+  asyncListObjects(bucketName, options) {
+    return RNAliyunOSS.asyncListObjects(bucketName, options);
+  },
 
+  /**
+   * Asynchronously asyncCopyObject
+   */
 
-    /**
-     * Asynchronously asyncCopyObject
-     */
+  asyncCopyObject(
+    srcBucketName,
+    srcObjectKey,
+    desBucketName,
+    destObjectKey,
+    options,
+  ) {
+    return RNAliyunOSS.asyncCopyObject(
+      srcBucketName,
+      srcObjectKey,
+      desBucketName,
+      destObjectKey,
+      options,
+    );
+  },
 
-    asyncCopyObject (srcBucketName, srcObjectKey, desBucketName,destObjectKey, options) {
-        return RNAliyunOSS.asyncCopyObject (srcBucketName, srcObjectKey, desBucketName,destObjectKey, options)
-    },
+  /**
+   * Asynchronously doesObjectExist
+   */
 
-    /**
-     * Asynchronously doesObjectExist
-     */
+  doesObjectExist(bucketName, objectKey) {
+    return RNAliyunOSS.doesObjectExist(bucketName, objectKey);
+  },
 
-     doesObjectExist (bucketName, objectKey) {
-        return RNAliyunOSS.doesObjectExist(bucketName, objectKey)
-     },
+  /**
+   * Asynchronously asyncDeleteObject
+   */
 
-     /**
-     * Asynchronously asyncDeleteObject
-     */
+  asyncDeleteObject(bucketName, objectKey) {
+    return RNAliyunOSS.asyncDeleteObject(bucketName, objectKey);
+  },
 
-     asyncDeleteObject (bucketName, objectKey) {
-        return RNAliyunOSS.asyncDeleteObject(bucketName, objectKey)
-     },
+  /**
+   * Asynchronously createBucket
+   */
+  asyncCreateBucket(bucketName, acl = 'private', region) {
+    return RNAliyunOSS.asyncCreateBucket(bucketName, acl, region);
+  },
 
-     /**
-     * Asynchronously createBucket
-     */
-    asyncCreateBucket (bucketName,acl="private",region) {
-        return RNAliyunOSS.asyncCreateBucket(bucketName,acl,region)
-    }, 
+  /**
+   * Asynchronously getBucketACL
+   */
+  asyncGetBucketACL(bucketName) {
+    return RNAliyunOSS.asyncGetBucketACL(bucketName);
+  },
 
-    /**
-     * Asynchronously getBucketACL
-     */
-    asyncGetBucketACL (bucketName) {
-        return RNAliyunOSS.asyncGetBucketACL(bucketName)
-    },
+  /**
+   * Asynchronously getBucketACL
+   */
+  asyncGetBucketACL(bucketName) {
+    return RNAliyunOSS.asyncGetBucketACL(bucketName);
+  },
 
-    /**
-     * Asynchronously getBucketACL
-     */
-    asyncGetBucketACL(bucketName) {
-        return RNAliyunOSS.asyncGetBucketACL(bucketName)
-    },
+  /**
+   * Asynchronously deleteBucket
+   */
+  asyncDeleteBucket(bucketName) {
+    return RNAliyunOSS.asyncDeleteBucket(bucketName);
+  },
 
-    /**
-     * Asynchronously deleteBucket
-     */
-    asyncDeleteBucket (bucketName) {
-        return RNAliyunOSS.asyncDeleteBucket(bucketName)
-    },   
-
-    /**
-     * event listener for native upload/download event
-     * @param event one of 'uploadProgress' or 'downloadProgress'
-     * @param callback a callback function accepts one params: event
-     */
-    addEventListener(event, callback) {
-        const RNAliyunEmitter = Platform.OS === 'ios' ? new NativeEventEmitter(RNAliyunOSS) : DeviceEventEmitter;
-        switch (event) {
-            case 'uploadProgress':
-                subscription = RNAliyunEmitter.addListener(
-                    'uploadProgress',
-                    e => callback(e)
-                );
-                break;
-            case 'downloadProgress':
-                subscription = RNAliyunEmitter.addListener(
-                    'downloadProgress',
-                    e => callback(e)
-                );
-                break;
-            default:
-                break;
-        }
-    },
-
-    /**
-     * remove event listener for native upload/download event
-     * @param event one of 'uploadProgress' or 'downloadProgress'
-     */
-    removeEventListener(event) {
-        switch (event) {
-            case 'uploadProgress':
-                subscription.remove();
-                break;
-            case 'downloadProgress':
-                subscription.remove();
-                break;
-            default:
-                break;
-        }
+  /**
+   * event listener for native upload/download event
+   * @param event one of 'uploadProgress' or 'downloadProgress'
+   * @param callback a callback function accepts one params: event
+   */
+  addEventListener(event, callback) {
+    const RNAliyunEmitter =
+      Platform.OS === 'ios'
+        ? new NativeEventEmitter(RNAliyunOSS)
+        : DeviceEventEmitter;
+    switch (event) {
+      case 'uploadProgress':
+        subscription = RNAliyunEmitter.addListener('uploadProgress', (e) =>
+          callback(e),
+        );
+        break;
+      case 'downloadProgress':
+        subscription = RNAliyunEmitter.addListener('downloadProgress', (e) =>
+          callback(e),
+        );
+        break;
+      default:
+        break;
     }
+  },
+
+  /**
+   * remove event listener for native upload/download event
+   * @param event one of 'uploadProgress' or 'downloadProgress'
+   */
+  removeEventListener(event) {
+    switch (event) {
+      case 'uploadProgress':
+        subscription.remove();
+        break;
+      case 'downloadProgress':
+        subscription.remove();
+        break;
+      default:
+        break;
+    }
+  },
 };
--- react-native-oss.podspec
+++ react-native-oss.podspec
@@ -2,13 +2,13 @@ require 'json'
 package = JSON.parse(File.read(File.join(__dir__, 'package.json')))
 
 Pod::Spec.new do |s|
-  s.name          = package['name']
+  s.name          = 'react-native-oss'
   s.version       = package["version"]
   s.summary       = package['description']
   s.author        = { 'albertluo' => 'luozhang002@126.com' }
   s.license       = package['license']
   s.homepage      = package['homepage']
-  s.source        = { :git => 'https://github.com/aliyun/aliyun-oss-react-native.git' }
+  s.source        = { :git => 'https://github.com/hengkx/react-native-oss.git' }
   s.platform      = :ios, '8.0'
 
   s.source_files  = 'ios/*.{h,m}'
